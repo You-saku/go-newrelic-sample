23fca9b94ec26a579a986565ddf124f5bc6167b6
You-saku <yousaku.dev19@gmail.com>
2025-12-12 00:24:48 +0900

    gin導入

--- api/index.go
+++ api/index.go
@@ -1,10 +1,26 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+var (
+	app *gin.Engine
+)
+
+func HealthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+	})
+}
+
+func init() {
+	app = gin.New()
+	app.GET("/health", HealthCheck)
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	app.ServeHTTP(w, r)
 }
