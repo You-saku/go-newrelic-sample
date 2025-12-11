8982868544129afe28b9f3e0f8bd8f89e145fde1
You-saku <yousaku.dev19@gmail.com>
2025-12-12 00:11:09 +0900

    chore: index.goファイルをフォーマット

--- api/index.go
+++ api/index.go
@@ -1,11 +1,10 @@
 package handler
- 
-import (
-  "fmt"
-  "net/http"
-)
- 
-func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-}
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+}
